fa4452226429461c5dbae66b37bd38acf532acd9
Aldo Santos <aldoacesso@gmail.com>
2026-06-18 14:09:55 -0300

    feat: Created ImcTable component for information table.

--- src/components/ImcCalc/ImcCalc.jsx
+++ src/components/ImcCalc/ImcCalc.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import Button from "../Button/Button";
+
 import "./ImcCalc.css";
 
-const ImcCalc = () => {
+const ImcCalc = ({ calcImc }) => {
   const [height, setHeight] = useState("");
   const [weight, setWeight] = useState("");
 
@@ -57,7 +58,7 @@ const ImcCalc = () => {
           </div>
         </div>
         <div className="action-control">
-          <Button id="calc-btn" text="Calcular" />
+          <Button id="calc-btn" text="Calcular" action={calcImc} />
           <Button id="clear-btn" text="Limpar" action={clearForm} />
         </div>
       </form>
---
+++ src/components/ImcTable/ImcTable.jsx
@@ -0,0 +1,11 @@
+import "./ImcTable.css";
+
+const ImcTable = () => {
+  return (
+    <>
+      <h1>Tabela</h1>
+    </>
+  );
+};
+
+export default ImcTable;
